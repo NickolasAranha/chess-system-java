0160e3c3a964d5c87d688fbfccda09e591511036
Nickolas Aranha <aranha.nickolas@gmail.com>
2023-03-30 14:57:32 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -56,6 +56,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
 	}
 		private void initialSetup() {
 			placeNewPiece('a', 8, new Rook(board, Color.WHITE));
