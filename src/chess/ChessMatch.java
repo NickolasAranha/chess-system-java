326a1ce9431960e07778a5ce9b126bc583876acb
Nickolas Aranha <aranha.nickolas@gmail.com>
2023-03-30 18:00:54 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -12,13 +12,33 @@ public class ChessMatch {
 	
 	private Board board;
 	
+	private int turn;
+	
+	private Color currentPlayer;
+	
+	
 	
 	Scanner sc = new Scanner(System.in);
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
 	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	
+	public void setCurrentPlayer(Color currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i=0; i<board.getRows();i++) {
@@ -50,6 +70,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 
@@ -64,6 +85,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
+		}
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
@@ -74,7 +98,11 @@ public class ChessMatch {
 				throw new ChessException("The chosen piece can't move to target position");
 			}
 		}
-
+		
+		private void nextTurn() {
+			turn ++;
+			currentPlayer = (currentPlayer == Color.WHITE) ?  Color.BLACK : Color.WHITE;
+		}
 
 		private void initialSetup() {
 			placeNewPiece('a', 8, new Rook(board, Color.WHITE));
